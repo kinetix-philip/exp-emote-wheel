d5807fb12acbe0a9c73efb21678487be08127be7
Gabriel <gabriel@kinetix.tech>
2022-06-22 16:24:46 +0200

    New EmotePlacement WP

---
+++ Kinetix_EmoteWheel/Assets/Scripts/WheelEmplacement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelEmplacement : MonoBehaviour
+{
+    private int emoteIndex;
+
+	public int EmoteIndex { get => emoteIndex; set => emoteIndex = value; }
+}
